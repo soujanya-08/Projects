363d72be597f710cc4676615f51c4b4f0629058e
soujanya-08 <soujanyatodkar@gmail.com>
2024-11-02 21:10:30 +0530

    created pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import { SignIn } from "./pages/SignIn"
+import { SignUp } from "./pages/SignUp"
+import { Dashboard } from "./pages/Dashboard"
+import { Projects } from "./pages/Projects"
+import { About } from "./pages/About"
 function App() {
   return (
-    <>
-      <div>
-        Blog
-      </div>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/sign-in" element={<SignIn/>}></Route>
+      <Route path="/sign-up" element={<SignUp/>}></Route>
+      <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/projects" element={<Projects/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
