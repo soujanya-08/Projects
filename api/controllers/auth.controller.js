d261077aa5267cbf72778272019e1e482c63f4c6
soujanya-08 <soujanyatodkar@gmail.com>
2024-11-05 13:05:18 +0530

    sign up functionality added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,16 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 //signing up takes time, we have to wait and then send response 
 // so it shd be Asynchronous
-export const signup = async(req,res)=>{
+export const signup = async(req,res, next)=>{
     const {username, email,password}= req.body;
 
     if(!username|| !email || !password || username === '' || email === '' || password === '')
-        return res.status(400).json({message: 'All feilds are required'});
-
+    {
+        next(errorHandler(400, 'All fields are required'));
+    }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
         username,
@@ -20,7 +22,7 @@ export const signup = async(req,res)=>{
         res.json("signup successful");
      }
      catch(error){
-        res.status(500).json({message: error.message});
+        next(error);
      }
     
 }
