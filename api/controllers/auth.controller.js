5337f0b52cea4a5836abb668554a898e3ab2cf45
soujanya-08 <soujanyatodkar@gmail.com>
2024-11-08 12:27:12 +0530

    sign-in functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
-
+import jwt from 'jsonwebtoken';
 //signing up takes time, we have to wait and then send response 
 // so it shd be Asynchronous
 export const signup = async(req,res, next)=>{
@@ -25,6 +25,33 @@ export const signup = async(req,res, next)=>{
         next(error);
      }
     
+}
+export const signin = async(req,res,next)=>{
+    const{email, password} = req.body;
+    if( !email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+
+    }
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password); 
+        //converts password to hash and compares with existing user password
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid password'));
+        }
+        //save the encrypted value to cookie of browser and then use later to authenticate user.
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET); 
+        const {password:pass, ...rest} = validUser._doc;
+
+        res.status(200).cookie('access_token', token,{
+            httpOnly: true}).json(rest);
+        
+    }catch(error){
+        next(error);
+    }
 }
 
 
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -63,8 +63,6 @@ export default function FooterCom() {
                                 </Footer.Link>
                                 <Footer.Link
                                     href="#"
-                                    
-                                    
                                 >
                                     Terms &amp; Conditions
                                 </Footer.Link>
